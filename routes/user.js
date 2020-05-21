c3bb3d6a8349f8edb503671f9092b4f7c6bcd1c5
Adeola Apanisile <adexconly@gmail.com>
2020-05-21 05:16:46 -0600

    initial commit

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const {requireSignin, isAuth, isAdmin} = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-const {userById} = require("../controllers/user");
+const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
-router.get("/secret/:userId", requireSignin, isAuth, isAuth, (req, res) => {
-  res.json({
-    user: req.profile
-  });
+router.get('/secret', requireSignin, (req, res) => {
+    res.json({
+        user: 'got here yay'
+    });
 });
 
-router.param('userId', userById);
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
+router.param('userId', userById);
 
 module.exports = router;
